f2e1ff2581ff46babce926e4ff4033b1e0abee7c
ktjooho <kxjooho@gmail.com>
2019-02-05 23:46:31 +0900

    프로젝트 셋팅중

---
+++ backend/src/main/kotlin/main/App.kt
@@ -0,0 +1,13 @@
+package main
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args : Array<String>) {
+    runApplication<App>(*args) {
+
+    }
+}
